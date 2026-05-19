e7a00b7f6a6bd76cd518c890d0c49fe6a84f6969
Dr. Bike Sydney <diego@drbikesydney.com.au>
2026-05-19 00:25:12 +0000

    fix: no-cache en admin.html, SW v13 para limpiar cache del browser en modo normal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC = 'drbike-static-v12';
-const CACHE_PAGES  = 'drbike-pages-v12';
+const CACHE_STATIC = 'drbike-static-v13';
+const CACHE_PAGES  = 'drbike-pages-v13';
 
 const STATIC_ASSETS = [
   '/index.html',
